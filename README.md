153728744d9d85c70d0142d3a2f43494b93ba82b
Chris Mutzel <c.mutzel@f5.com>
2015-09-18 11:05:24 -0700

    cleanup

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 Run a docker container include hackazon, apache, and mysql
 
-This work is based on https://github.com/jbfink/docker-wordpress, but includes https://github.com/rapid7/hackazon instead of wordpress
+This work is based on https://github.com/jbfink/docker-wordpress, but includes https://github.com/rapid7/hackazon instead of wordpress.
 
 For more information on multiple processes inside of the same Docker container, see
 http://tech.paulcz.net/2014/12/multi-process-docker-images-done-right/
+
 
 # Instructions
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,10 +5,10 @@
 sleep 10s
 # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
 MYSQL_USER="root"
-MYSQL_PASSWORD="GoF5!"
+MYSQL_PASSWORD="hackmesilly"
 HACKAZON_DB="hackazon"
 HACKAZON_USER="hackazon"
-HACKAZON_PASSWORD='GoF5!'
+HACKAZON_PASSWORD='hackmesilly'
 
 #This is so the passwords show up in logs. 
 echo hackazon password: $HACKAZON_PASSWORD
@@ -18,7 +18,6 @@ echo $HACKAZON_PASSWORD > /hackazon-db-pw.txt
 #but thanks to @djfiander's thing at https://gist.github.com/djfiander/6141138
 #there isn't now.
 
-#chown www-data:www-data /var/www/wp-config.php
 mysqladmin -u root password $MYSQL_PASSWORD
 mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE $HACKAZON_DB; GRANT ALL PRIVILEGES ON $HACKAZON_DB.* TO '$HACKAZON_USER'@'localhost' IDENTIFIED BY '$HACKAZON_PASSWORD'; FLUSH PRIVILEGES;"
 killall mysqld
